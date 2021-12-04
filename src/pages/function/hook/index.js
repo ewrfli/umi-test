82d3f7fe49aa1458ccb37cb09859ce6da2cea481
ewrf3 <q304062@163.com>
2021-12-05 00:07:15 +0800

    hook test-01

---
+++ src/pages/function/hook/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+export default function(props){
+  const [count, setCount] = useState(10)
+
+  useEffect(() => {
+
+  }, [])
+
+  const handleCount = ()=>{
+    setCount(count + 1)
+  }
+  return (
+    <div>
+       <h1 onClick={handleCount}>count: {count}</h1>
+    </div>
+  )
+}
