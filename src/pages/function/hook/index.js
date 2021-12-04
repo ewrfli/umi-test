c00ad16dfc3eefdae6e96eba1edd9c436c937470
ewrf3 <q304062@163.com>
2021-12-05 00:42:04 +0800

    hook test-02

--- src/pages/function/hook/index.js
+++ src/pages/function/hook/index.js
@@ -1,18 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect, useMemo, useCallback } from 'react';
 
 export default function(props){
   const [count, setCount] = useState(10)
+  const [text, setText] = useState('text-demo')
+  //const [state, setState] = useState()
+
+
+  //useEffect api 异步相关操作 可多个
+  async function demo1(){
+    console.log('demo1 async')
+  }
 
   useEffect(() => {
+    fetch('/api/getLists')
+    console.log('useEffect')//支持异步
 
+    async function demo(){
+      console.log('demo async')
+    }
+    demo()//不需要await
+    demo1()
+  }, [count]) //参数[] 只执行一次 
+
+  //
+  useEffect(() => {
+    console.log('useEffect 2')
   }, [])
 
-  const handleCount = ()=>{
-    setCount(count + 1)
-  }
+  //useLayoutEffect api 操作dom 改变显示效果 
+  useLayoutEffect(() => {
+    console.log('useLayoutEffect')
+  }, [])
+
+  ////////
+
+  // const handleCount = ()=>{
+  //   setCount(count + 1)
+  // }
+
+  const noCacheText = ()=>{
+    console.log('text changed')
+    return text
+  }   
+
+  //useMeno, useCallback 缓存 性能优化 返回经过缓存的值
+  const memoText = useMemo(()=>{
+    console.log('text changed2')
+    return text
+  }, [text])
+
+  const handleCount = useCallback(()=>{
+      console.log('count changed')
+      setCount(count + 1)
+  }, [count])
+
   return (
     <div>
        <h1 onClick={handleCount}>count: {count}</h1>
+       {/* <h1>text:{noCacheText()}</h1> */}
+       <h1>text:{memoText}</h1>
     </div>
   )
 }
